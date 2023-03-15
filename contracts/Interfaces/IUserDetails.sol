0d8174382a6b1dca274a77861fc49dbfb4797f17
Deepak973 <deepak.rj.dr@gmail.com>
2023-03-16 01:34:05 +0530

    added setter & getter in userDetails, update uInterface

--- contracts/Interfaces/IUserDetails.sol
+++ contracts/Interfaces/IUserDetails.sol
@@ -2,15 +2,22 @@
 pragma solidity >=0.8.0 <=0.8.19;
 
 interface IUserDetails {
+
+   enum userType {
+    Supplier,
+    Manufacturer,
+    Distributor
+  } 
   struct userDetails{
-      bytes userType;
+      userType userType;
       bytes userName;
       bytes userPhysicalAddress;
       bytes userImage;
   }
-  function addUser(bytes calldata _type, bytes calldata _name, bytes calldata _physicalAddress,bytes memory _image)external;
-  function deleteUser(bytes memory _address)external;
 
+ 
+  function addUser(userType _type, bytes calldata _name, bytes calldata _physicalAddress,bytes memory _image)external;
+  function deleteUser(address _address)external;
   function editName(bytes memory _name)external;
   function editPhysicalAddress(bytes memory _physicalAddress)external;
   function editImage(bytes memory _image)external;
--- contracts/userDetails.sol
+++ contracts/userDetails.sol
@@ -6,25 +6,78 @@ import "./Interfaces/IUserDetails.sol" ;
 contract userDetails is IUserDetails{
 
     mapping(address=>userDetails) public userDetailsMapping;
+    address[] public users;
+    address[] public suppliers;
+    address[] public manufacturers;
+    address[] public distributors;
 
-    function addUser(bytes calldata _type, bytes calldata _name, bytes calldata _physicalAddress,bytes memory _image)external{
+    
 
-        // userDetailsMapping
+    function addUser(userType _type, bytes calldata _name, bytes calldata _physicalAddress,bytes memory _image)public{
+
+         require((userDetailsMapping[msg.sender].userName).length == 0, "User already registered");
+         userDetailsMapping[msg.sender] = userDetails(_type,_name,_physicalAddress,_image);
+         users.push(msg.sender);
+         if(_type == userType.Supplier) suppliers.push(msg.sender);
+         if(_type == userType.Manufacturer) manufacturers.push(msg.sender);
+         if(_type == userType.Distributor) distributors.push(msg.sender);
+         
 
     }
-    function deleteUser(bytes memory _address)external{
+    function deleteUser(address _address)public{
+        delete userDetailsMapping[_address];
 
     }
 
-    function editName(bytes memory _name)external{
+    function editName(bytes memory _name)public{
+        userDetailsMapping[msg.sender].userName = _name;
 
     }
-    function editPhysicalAddress(bytes memory _physicalAddress)external{
+    function editPhysicalAddress(bytes memory _physicalAddress)public{
+        userDetailsMapping[msg.sender].userPhysicalAddress = _physicalAddress;
 
     }
-    function editImage(bytes memory _image)external{
+    function editImage(bytes memory _image)public{
+        userDetailsMapping[msg.sender].userImage = _image;
+    }
+
+    function getAllUsers() public view returns(userDetails[] memory){
+        userDetails[] memory userD = new userDetails[](users.length);
+        for(uint i=0;i<users.length;i++)
+        {
+            userD[i] = userDetailsMapping[users[i]];
+        }
+        return userD;
+    }
+
+    function getAllSuppliers() public view returns(userDetails[] memory){
+        userDetails[] memory suppD = new userDetails[](suppliers.length);
+        for(uint i=0;i<users.length;i++)
+        {
+            suppD[i] = userDetailsMapping[suppliers[i]];
+        }
+        return suppD;
 
     }
 
+      function getAllManufacturers() public view returns(userDetails[] memory){
+        userDetails[] memory manuD = new userDetails[](manufacturers.length);
+        for(uint i=0;i<users.length;i++)
+        {
+            manuD[i] = userDetailsMapping[manufacturers[i]];
+        }
+        return manuD;
+
+    }
+
+      function getAllDistributors() public view returns(userDetails[] memory){
+        userDetails[] memory distD = new userDetails[](distributors.length);
+        for(uint i=0;i<users.length;i++)
+        {
+            distD[i] = userDetailsMapping[distributors[i]];
+        }
+        return distD;
+
+    }
 
 }
