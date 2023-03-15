e0adea8a944eeed043f256c349e58b128e6d76d5
Deepak973 <deepak.rj.dr@gmail.com>
2023-03-15 20:15:09 +0530

    once again added userDetails.sol

--- contracts/userDetails.sol
+++ contracts/userDetails.sol
@@ -1,6 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
-import "./Interfaces/IUserDetails.sol";
 pragma solidity >=0.8.0 <=0.8.19;
-contract userDetails{
-    
+
+import "./Interfaces/IUserDetails.sol" ;
+
+contract userDetails is IUserDetails{
+
+    mapping(address=>userDetails) public userDetailsMapping;
+
+    function addUser(bytes calldata _type, bytes calldata _name, bytes calldata _physicalAddress,bytes memory _image)external{
+
+        // userDetailsMapping
+
+    }
+    function deleteUser(bytes memory _address)external{
+
+    }
+
+    function editName(bytes memory _name)external{
+
+    }
+    function editPhysicalAddress(bytes memory _physicalAddress)external{
+
+    }
+    function editImage(bytes memory _image)external{
+
+    }
+
+
 }
